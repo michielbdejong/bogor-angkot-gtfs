668c6031b441d14f0e9af19dc6c0ab0bbd208bce
Michiel de Jong <michiel@unhosted.org>
2016-07-28 15:27:05 +0700

    Also extract places list from majalah and 2menit

--- data-from-www/bogor-2menit-com.js
+++ data-from-www/bogor-2menit-com.js
@@ -2,19 +2,20 @@ var fs = require('fs');
 
 var lines = fs.readFileSync('bogor-2menit-com.txt').toString().split('\n\n');
 
+var places = {};
 var routes = {};
 
 for (var i=0; i<lines.length; i++) {
   var cells = lines[i].split('\n');
   var firstLineParts = cells[0].split(', Jurusan: ');
-  try {
-    routes[firstLineParts[0].substring('Trayek '.length)] = {
-      name: firstLineParts[1].trim(),
-      stops: cells[1].substring('Rute: '.length).split(/[-–]/).map(stop => stop.trim())
-    };
-  } catch (e) {
-    routes[firstLineParts[0].substring('Trayek '.length)] = cells;
+  var lineNo = firstLineParts[0].substring('Trayek '.length);
+  routes[lineNo] = {
+    name: firstLineParts[1].trim(),
+    stops: cells[1].substring('Rute: '.length).split(/[-–]/).map(stop => stop.trim())
+  };
+  for (var j=0; j<routes[lineNo].stops.length; j++) {
+    places[routes[lineNo].stops[j]] = true;
   }
 }
 
-console.log(JSON.stringify({routes}, null, 2));
+console.log(JSON.stringify({ places: Object.keys(places).sort(), routes}, null, 2));
--- data-from-www/majalahtransportasi-com.js
+++ data-from-www/majalahtransportasi-com.js
@@ -2,19 +2,20 @@ var fs = require('fs');
 
 var lines = fs.readFileSync('majalahtransportasi-com.txt').toString().split('\n\n');
 
+var places = {};
 var routes = {};
 
 for (var i=0; i<lines.length; i++) {
   var cells = lines[i].split('\n');
-  try {
-    routes[cells[0]] = {
-      name: cells[1].substring('Trayek: '.length),
-      colour: cells[2].substring('Warna: '.length),
-      stops: cells[3].substring('Rute: '.length).split(/[-–]/).map(stop => stop.trim())
-    };
-  } catch (e) {
-    routes[cells[0]] = cells;
+  var lineNo = cells[0].trim();
+  routes[lineNo] = {
+    name: cells[1].substring('Trayek: '.length),
+    colour: cells[2].substring('Warna: '.length),
+    stops: cells[3].substring('Rute: '.length).split(/[-–]/).map(stop => stop.trim())
+  };
+  for (var j=0; j< routes[lineNo].length; j++) {
+    places[routes[lineNo].stops[j]] = true;
   }
 }
 
-console.log(JSON.stringify({routes}, null, 2));
+console.log(JSON.stringify({ places: Object.keys(places).sort(), routes}, null, 2));
