4b95cbfa8667a538e48a50fd3181feb9a68eab5d
Michiel de Jong <michiel@unhosted.org>
2016-07-29 17:48:13 +0700

    Draw sources in different colours in per-line canvasas

--- makeCanvases.js
+++ makeCanvases.js
@@ -2,5 +2,5 @@ var fs = require('fs');
 
 var joined = JSON.parse(fs.readFileSync('./build/joined.json'));
 for (var lineNo in joined.routes) {
-  console.log(`    <canvas id="canvas-${lineNo}" width="1000" height="1000" style="border:1px solid #000000;"></canvas>`);
+  console.log(`<h2>${lineNo}</h2><canvas id="canvas-${lineNo}" width="1000" height="1000" style="border:1px solid #000000;"></canvas>`);
 }
