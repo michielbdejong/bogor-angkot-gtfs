67a94335800781b579c23ecfd4c9aa90b8d764b2
Michiel de Jong <michiel@unhosted.org>
2016-08-11 14:23:18 +0700

    Fix shape_id in trips.txt

--- gen-gtfs.js
+++ gen-gtfs.js
@@ -77,7 +77,7 @@ fs.writeFileSync('release/routes.txt', headers.routes + '\n' +
 //  stops: 'stop_id,stop_code,stop_name,stop_desc,stop_lat,stop_lon,zone_id,stop_url,location_type,parent_station,stop_timezone,wheelchair_boarding',
 fs.writeFileSync('release/stops.txt', headers.stops+'\n'+stopsArr.join('\n'));
 //  trips: 'route_id,service_id,trip_id,trip_headsign,trip_short_name,direction_id,block_id,shape_id,wheelchair_accessible,bikes_allowed',
-fs.writeFileSync('release/trips.txt', headers.trips + '\n' + trips.map(name => `${name},FULL,${name},${name},,,,,2,2`).join('\n') + '\n');
+fs.writeFileSync('release/trips.txt', headers.trips + '\n' + trips.map(name => `${name},FULL,${name},${name},,,,${name},2,2`).join('\n') + '\n');
 //  frequencies: 'trip_id,start_time,end_time,headway_secs',
 fs.writeFileSync('release/frequencies.txt', headers.frequencies + '\n' +
   trips.map(name => `${name},06:00:00,24:00:00,60,0`).join('\n') + '\n');
