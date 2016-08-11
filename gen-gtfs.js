05b27b2060687a5e06bd554cde9d8d18d04fa86a
Michiel de Jong <michiel@unhosted.org>
2016-08-11 13:11:11 +0700

    fixed files

--- gen-gtfs.js
+++ gen-gtfs.js
@@ -17,7 +17,7 @@ var headers = {
 var fixed = {
   agency: 'DLLAJ,Pemerintah Kota Bogor angkotan kota,http://kotabogor.go.id/index.php/rute,Asia/Jakarta',
   calendar: 'FULL,1,1,1,1,1,1,1,20160101,20251231',
-  fare_attributes: ' biasa,3500,IDR,0,0,\nsiswa,2500.25,IDR,0,0,\nkabupaten,6000,IDR,0,0,',
+  fare_attributes: 'biasa,3500,IDR,0,0,\nsiswa,2500.25,IDR,0,0,\nkabupaten,6000,IDR,0,0,',
 };
 
 var data = fs.readFileSync('manual-from-transitwand/kota/shapes.txt').toString().split('\n').map(line => line.split(','));
@@ -63,6 +63,9 @@ for (var i=1; i<data.length; i++) {
   stopTimes.push([line[0],toTime(seq[line[0]]),toTime(seq[line[0]]),getStop(line[1], line[2], line[5]), seq[line[0]]]);
 }
 
+for (var fileName in fixed) {
+  fs.writeFileSync(`release/${fileName}.txt`, headers[fileName]+'\n'+fixed[fileName]+'\n');
+}
 fs.writeFileSync('release/stops.txt', headers.stops+'\n'+stopsArr.join('\n'));
 fs.writeFileSync('release/trips.txt', headers.trips+'\n'+trips.map(name => `${name},0,${name}`).join('\n')+'\n');
 fs.writeFileSync('release/routes.txt', headers.routes+'\n'+trips.map(name => `${name},0,${name}`).join('\n')+'\n');
