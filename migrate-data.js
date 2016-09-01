3bf6153575de1cfd4e36c300902992c4f9edf407
Michiel de Jong <michiel@unhosted.org>
2016-09-01 15:49:42 +0700

    skip stretches table

--- migrate-data.js
+++ migrate-data.js
@@ -1,30 +1,26 @@
 var fs = require('fs');
 var mathjs = require('mathjs');
-var stretches = [];
-var lanes = [];
+var numLanes = {};
 var lastLine;
-
-function processStretch(from, to) {
-  var stretchDef = [from[1], from[2], to[1], to[2]].join(',');
-  if (typeof stretches[stretchDef] === 'undefined') {
-    stretches[stretchDef] = [];
-  }
-  stretches[stretchDef].push(from[0]);
-}
+const NAME_COL = 0;
+const LAT_COL = 1;
+const LON_COL = 2;
 
 
 //...
 fs.readFileSync('./manual-from-transitwand/kota/shapes.txt').toString().split('\n').map(line => {
   var columns = line.split(',');
-  if (lastLine && lastLine[0] == columns[0]) {
-    processStretch(lastLine, columns);
+  if (lastLine && lastLine[NAME_COL] == columns[NAME_COL]) {
+    processStretch(columns[NAME_COL],
+                   parseFloat(lastLine[LAT_COL]), parseFloat(lastLine[LON_COL]),
+                   parseFloat(columns[LAT_COL]), parseFloat(columns[LON_COL]));
   }
   lastLine = columns;
 });
 
-function perpendicularVector(coords) {
-  var distance = mathjs.distance([coords[0], coords[1]], [coords[2], coords[3]]);
-  var normalized = [(coords[2]-coords[0])/distance, (coords[3]-coords[1])/distance];
+function perpendicularVector(fromX, fromY, toX, toY) {
+  var distance = mathjs.distance([fromX, fromY], [toX, toY]);
+  var normalized = [(toX-fromX)/distance, (toY-fromY)/distance];
   // NE, shift NW
   // SE, shift NE
   // East, shift North
@@ -34,19 +30,19 @@ function perpendicularVector(coords) {
   // North, shift West
   // South, shift East
   // Stationary, don't shift
-  return [-normalized[1], normalized[0]);
+  return [-normalized[1], normalized[0]];
 }
 
-
-for (var stretchDef in stretches) {
-  var coords = stretchDef.split(',').map(x => parseFloat(x));
-  var laneVector = perpendicularVector(coords);
-  for (var lane = 0; lane<stretches[stretchDef].length;lane++) {
-    var start = [coords[0] + (lane+1) * laneVector[0],
-                 coords[1] + (lane+1) * laneVector[1]];
-    var finish = [coords[2] + (lane+1) * laneVector[0],
-                  coords[3] + (lane+1) * laneVector[1]];
-    var routeName = stretches[stretchDef][lane];
-    console.log(`${routeName},${start[0]},${start[1]},${finish[0]},${finish[1]}`);
+function processStretch(routeName, fromX, fromY, toX, toY) {
+  var laneVector = perpendicularVector(fromX, fromY, toX, toY);
+  var stretchDef = [fromX, fromY, toX, toY].join(',');
+  if (typeof numLanes[stretchDef] === 'undefined') {
+    numLanes[stretchDef] = 0;
   }
+  var lane = ++numLanes[stretchDef];
+  var laneFromX = fromX + lane * laneVector[0];
+  var laneFromY = fromY + lane * laneVector[1];
+  var laneToX = toX + lane * laneVector[0];
+  var laneToY = toY + lane * laneVector[1];
+  console.log([routeName, laneFromX, laneFromY, laneToX, laneToY].join(','));
 }
