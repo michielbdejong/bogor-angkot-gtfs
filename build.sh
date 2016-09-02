0cefe109709bb77b2c739c56bfb8c160ea21b0af
Michiel de Jong <michiel@unhosted.org>
2016-09-02 13:31:11 +0700

    Temporarily remove currently unused build steps, attempt to align line 05

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
-cd manual-from-transitwand/kota
-zip -r ../../build/kota.zip *
-cd ../kabupaten
-zip -r ../../build/kabupaten.zip *
-cd ../../data-from-www
-node ./lovelybogor-com.js > ../build/lovelybogor.json
-node ./kotabogor-go-id.js > ../build/kotabogor.json
-node ./majalahtransportasi-com.js > ../build/majalah.json
-node ./bogor-2menit-com.js > ../build/2menit.json
-node ./enterbogor-blogspot-co-id.js > ../build/enterbogor.json
-cd ..
-node ./joinData.js > build/joined.json
-node ./make-gtfs.js > release/shapes.txt
-node ./gen-gtfs.js
-cd release
-zip -r ../release.zip *
-cd ..
-node listAllStopsPerRoute.js > build/stopsPerRoute.json
-node makeTables.js > tables.html
+# cd manual-from-transitwand/kota
+# zip -r ../../build/kota.zip *
+# cd ../kabupaten
+# zip -r ../../build/kabupaten.zip *
+# cd ../../data-from-www
+# node ./lovelybogor-com.js > ../build/lovelybogor.json
+# node ./kotabogor-go-id.js > ../build/kotabogor.json
+# node ./majalahtransportasi-com.js > ../build/majalah.json
+# node ./bogor-2menit-com.js > ../build/2menit.json
+# node ./enterbogor-blogspot-co-id.js > ../build/enterbogor.json
+# cd ..
+# node ./joinData.js > build/joined.json
+# node ./make-gtfs.js > release/shapes.txt
+# node ./gen-gtfs.js
+# cd release
+# zip -r ../release.zip *
+# cd ..
+# node listAllStopsPerRoute.js > build/stopsPerRoute.json
+# node makeTables.js > tables.html
 node --harmony_destructuring migrate-data.js > build/stretches.txt
