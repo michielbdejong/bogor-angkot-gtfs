4bebe90b101a6ad646852d19de594bdfc4a3386d
Michiel de Jong <michiel@unhosted.org>
2016-10-17 17:53:49 +0700

    Update my publish.sh script to account for https://github.com/michielbdejong/bogor-angkot-gtfs/issues/15#issuecomment-254175427

--- publish.sh
+++ publish.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cp release/map.svg ../michielbdejong.com/angkots.svg
+cp release/gtfs.zip ../michielbdejong.com/angkots-gtfs.zip
 cd ../michielbdejong.com
-git commit -am"Update angkots map"
+git commit -am"Update angkots map and gtfs"
 git push
 cd ../bogor-angkot-gtfs
